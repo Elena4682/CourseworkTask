ec869b5d232b1b1ef9381503c6873f9226cd9c79
Елена <simonaitene@yandex.ru>
2023-03-13 21:36:51 +0300

    Задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] numbers = {1,2,3,4,5,6};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i != numbers.length - 1) {
+                System.out.print(" , ");
+            }
+        }
+        System.out.println();
+        for (int i = numbers.length-1; i >=0; i--) {
+            System.out.print(numbers[i]);
+            if(i!=0){
+                System.out.print(" , ");
+            }
+        }
     }
 }
+
